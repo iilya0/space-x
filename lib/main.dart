4ef7740e75d72fb7ff89972d5ec8c25f88848099
iilya0 <osipovilya549@gmail.com>
2025-09-09 09:26:18 +0300

    configure HomeBloc, initialize repository/service

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:space_x/src/core/resources/app_colors.dart';
+import 'package:space_x/src/features/home/presentation/screens/home_screen.dart';
+import 'package:space_x/src/features/home/presentation/bloc/home_bloc.dart';
+import 'package:space_x/src/features/home/presentation/bloc/home_event.dart';
+import 'package:space_x/src/features/home/domain/home_repository.dart';
+import 'package:space_x/src/features/home/data/home_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,57 +16,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return BlocProvider(
+      create: (context) =>
+          HomeBloc(repository: HomeRepository(HomeService()))
+            ..add(const HomeLoadRockets()),
+      child: MaterialApp(
+        title: 'SpaceX Launches',
+        themeMode: ThemeMode.dark,
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: AppColors.black,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: AppColors.black,
+            foregroundColor: AppColors.white,
+          ),
+          colorScheme: const ColorScheme.dark(
+            primary: AppColors.white,
+            surface: AppColors.black,
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        home: const HomeScreen(),
       ),
     );
   }
